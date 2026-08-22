71a1c5babf54fbd475e3eed0f07830eec8478e38
Andy Slater <aslater3@googlemail.com>
2026-08-22 05:28:42 +0100

    fix: replace generated musl loader symlink

--- build/ci/build-public-toolchain.sh
+++ build/ci/build-public-toolchain.sh
@@ -23,6 +23,7 @@ cp -a "$OUT/arm-linux-musleabihf/include" "$OUT/usr/armv7-alpine-linux-musleabih
 for library in /lib/x86_64-linux-gnu/libgmp.so.10 /lib/x86_64-linux-gnu/libmpfr.so.6 /lib/x86_64-linux-gnu/libmpc.so.3 /usr/lib/x86_64-linux-gnu/libisl.so.23 /usr/lib/x86_64-linux-gnu/libz.so.1 /usr/lib/x86_64-linux-gnu/libzstd.so.1 /usr/lib/x86_64-linux-gnu/libjansson.so.4; do
   cp -- "$library" "$OUT/usr/lib/$(basename "$library")"
 done
+rm -f "$OUT/usr/lib/ld-musl-armhf.so.1"
 cp -- "$OUT/arm-linux-musleabihf/lib/libc.so" "$OUT/usr/lib/ld-musl-armhf.so.1"
 cp -- /lib/ld-musl-x86_64.so.1 "$OUT/lib/ld-musl-x86_64.so.1"
 for tool in ar as c++ cc cpp elfedit gcc gcc-ar gcc-nm gcc-ranlib g++ ld ld.bfd nm objcopy objdump ranlib readelf size strings strip; do
